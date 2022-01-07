ae2ab722fb7fbe5ed6090be9c61ad508e53ee4ca
borgepl <pdeborger@gmail.com>
2022-01-07 14:05:57 +0100

    Added refresh-database-with-100-products.sql

--- src/main/java/com/deborger/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/deborger/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.deborger.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,4 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
